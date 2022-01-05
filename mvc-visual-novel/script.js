3d84dfa670001101ce43624404cb654a3a6967ce
KValexander <ewoase@gmail.com>
2022-01-05 14:49:54 +0300

    mvc upd

---
+++ mvc-visual-novel/script.js
@@ -0,0 +1,9 @@
+// Onload
+window.addEventListener("load", function() {
+	// Calculating the minimum content height
+	height = document.documentElement.scrollHeight;
+	head_height = document.querySelector("header").clientHeight;
+	foot_height = document.querySelector("footer").clientHeight;
+	result = height - (head_height + foot_height) - 3;
+	document.querySelector("main").style["min-height"] = `${result}px`;
+});
